9c350678f344bfb11a0c3417a3d90c12f6e2d6e5
Dan VIAL <dan.vial@dbmail.com>
2024-06-03 15:28:29 +0200

    bug git commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,16 @@ export default defineConfig({
       'top-level-await': true
     },
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@model': path.resolve(__dirname, './src/data'),
+      '@view': path.resolve(__dirname, './src/components'),
+      '@controller': path.resolve(__dirname, './src/routes'),
+    },
+  },
+  // server: {
+  //   https: true
+  // },
 })
